8f52c2b2c6a4a1895bdd01a22e207621aa7f3d30
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2026-07-01 20:15:42 +0530

    user model added

---
+++ src/models/user.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+let UserSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    username : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        trim : true,
+        index : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    // this is best practise
+    status : {
+        type : String,
+        default : "offline",
+        enum : {
+            values : ["offline","online","away"],
+            message: `{VALUE} is not a valid status`
+        }
+    },
+    bio : {
+        type : String,
+        default : "Hey there, Looking for matches !!!"
+    },   
+    lastseen : {
+        type : Date
+    },
+    avatar : {
+        type : String,
+        default : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT0M9PkaDKnCMW8NANGmmvjkS-WhhsIOe4pQ&s",
+    }
+},{timestamps:true});
+
+let UserModel = new mongoose.model("users");
+export default UserModel;
+
