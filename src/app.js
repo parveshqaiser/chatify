50fc20df4e782169a9c59b4dba4c649a203e5be7
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2026-07-05 22:15:34 +0530

    login route with cookie added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
+
+import "dotenv/config";
+
 import express, { urlencoded } from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+
 import dbConnection from "./config/db.js";
 import authRoutes from "./routes/auth.routes.js"; 
+import cookieParser from "cookie-parser";
 
 let app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
 
 app.use(cors({
     origin : "*",
