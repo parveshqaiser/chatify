c923eae934ff60d7e842e352cd51d465aa5581c5
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2026-07-08 21:02:50 +0530

    added condtion for already registered

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -6,7 +6,7 @@ export let generateEmailVerificationToken = ()=>{
     let unhashedToken = crypto.randomBytes(10).toString("hex");
     let hashedToken = crypto.createHash("sha256").update(unhashedToken).digest("hex");
 
-    let tokenExpiry = Date.now() +(5*60*1000);
+    let tokenExpiry = Date.now() +(2*60*1000);  // 2 mins
 
     return {unhashedToken,hashedToken, tokenExpiry};
 }
