907ea5b095aec7cd375fe6fc7babc108c364cba9
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2026-07-16 23:32:48 +0530

    del msg controller added

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -1,12 +1,13 @@
 
 import express from "express";
 import userAuthentication from "../middlewares/auth.middleware.js";
-import { getAllMessage, sendMessage } from "../controllers/chat.controller.js";
+import { deleteMessage, getAllMessage, sendMessage } from "../controllers/chat.controller.js";
 
 const router = express.Router();
 
 router.post("/send/:id", userAuthentication, sendMessage);
 router.get("/:id", userAuthentication, getAllMessage);
+router.delete("/:id/:msgId", userAuthentication, deleteMessage);
 
 
 export default router;
