3d37da21d27af260e1494be0150931e3137ab3b9
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2026-07-28 21:29:13 +0530

    app.js file modified

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 app.use(cors({
-    origin : "http://localhost:5173/",
+    origin : ["http://localhost:5173","http://localhost:5174"],
     methods : ["GET", "POST", "PUT", "PATCH","DELETE"],
     credentials : true,
     allowedHeaders : ["Content-Type", "Authorization"]
