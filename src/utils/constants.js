4cf3546c469e67196d6c659670ff07df33341e1c
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2026-08-04 21:19:40 +0530

    last login field added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,11 @@
 
 export let allowedDomains = ["@gmail.com", "@hotmail.com", "@yahoo.com"];
+
+export let generateDate =()=>{
+    return new Date().toLocaleString("en-IN", {
+        day: "numeric",
+        month: "short",
+        year: "numeric",
+        timeZone: "Asia/Kolkata"
+    });
+}
